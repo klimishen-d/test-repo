c32041f32aa4bc2fa3be76e398b39d06f355df1d
klimishen-d <d.klimishen@geniusee.com>
2024-10-03 14:10:12 +0300

    Refactor handleClick function and add form submission functionality

--- src/App.tsx
+++ src/App.tsx
@@ -8,28 +8,38 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
       <button className="App-button" onClick={handleClick}>
         Increment
       </button>
 
       <p>Count: {count}</p>
 
-      <button className="App-button">Reset</button>
+      <button onClick={() => setCount(0)} className="App-button">
+        Reset
+      </button>
+
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const formData = new FormData(e.target as any);
+            const formProps = Object.fromEntries(formData);
+            console.log(formProps);
+            alert("Check the console for the form data");
+          }}
+        >
+          <label
+            style={{ display: "flex", flexDirection: "column", width: 300 }}
+          >
+            Name:
+            <input type="text" name="name" />
+          </label>
+
+          <div>
+            <button type="submit">submit</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
